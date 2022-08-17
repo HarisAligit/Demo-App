3ba4a174aa9d26533cd88b6140d28bd1850d0ec0
Haris Ali <haris.ali@empglabs.com>
2022-08-17 17:47:13 +0500

    Added Navbar + fixed re render login issue

--- src/Components/Auth/Protected.js
+++ src/Components/Auth/Protected.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Navigate, Route } from "react-router-dom";
+import {Navigate, Outlet, Route} from "react-router-dom";
 
 const Protected = ({ children }) => {
   const isAuthenticated = localStorage.getItem("isAuthenticated");
 
-  if (isAuthenticated === "false") return <Navigate to="/" />;
-  else return children;
+  if (isAuthenticated === "false") return <Navigate to="/login" />;
+  else return <Outlet />;
 };
 
 export default Protected;
