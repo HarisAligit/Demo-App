c9aaf1b6e1e4ef89b5bf9a1d4ea5e073a7309ba7
Haris Ali <haris.ali@empglabs.com>
2022-08-17 20:12:17 +0500

    Minor Fix

--- src/Components/Client/Clients.js
+++ src/Components/Client/Clients.js
@@ -1,9 +1,9 @@
-import Table from 'react-bootstrap/Table';
-import {useGetClientsQuery} from "../../Redux/Slice/authSlice"
-import {useEffect} from "react";
+import Table from "react-bootstrap/Table";
+import { useGetClientsMutation } from "../../Redux/Slice/authSlice";
+import { useEffect } from "react";
 
 const Clients = () => {
-  const { data, error, isLoading } = useGetClientsQuery();
+  const { data, error, isLoading } = useGetClientsMutation();
   // const getData = async () => {
   //   try {
   //     await getClients().unwrap();
@@ -14,9 +14,7 @@ const Clients = () => {
   //   }
   // }
 
-  useEffect(() => {
-
-  }, [])
+  useEffect(() => {}, []);
 
   return (
     <Table striped bordered hover>
@@ -28,34 +26,34 @@ const Clients = () => {
         <> data </>
       ) : null}
       <thead>
-      <tr>
-        <th>#</th>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Username</th>
-      </tr>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
       </thead>
       <tbody>
-      <tr>
-        <td>1</td>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-      </tr>
-      <tr>
-        <td>2</td>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-      </tr>
-      <tr>
-        <td>3</td>
-        <td colSpan={2}>Larry the Bird</td>
-        <td>@twitter</td>
-      </tr>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
       </tbody>
     </Table>
   );
-}
+};
 
 export default Clients;
--- src/Redux/Slice/authSlice.js
+++ src/Redux/Slice/authSlice.js
@@ -1,37 +1,53 @@
-import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const jarvisApi = createApi({
-  reducerPath: 'jarvisApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://staging-olxpk.jarvisempg.com/api/crm/' }),
-  tagTypes: ['Post'],
+  reducerPath: "jarvisApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://staging-olxpk.jarvisempg.com/api/crm/",
+  }),
+  tagTypes: ["Post"],
   endpoints: (builder) => ({
     signIn: builder.mutation({
       query: (payload) => ({
-        url: '/auth/sign_in',
-        method: 'POST',
+        url: "/auth/sign_in",
+        method: "POST",
         body: payload,
         headers: {
-          'Content-type': 'application/json; charset=UTF-8',
+          "Content-type": "application/json; charset=UTF-8",
         },
       }),
       transformResponse(response, meta) {
-        return { response, accessToken: meta.response.headers.get('access-token'), client: meta.response.headers.get('client'), uid: meta.response.headers.get('uid')}
+        return {
+          response,
+          accessToken: meta.response.headers.get("access-token"),
+          client: meta.response.headers.get("client"),
+          uid: meta.response.headers.get("uid"),
+        };
       },
-      tagTypes: ['Post'],
+      tagTypes: ["Post"],
     }),
     getOpportunities: builder.mutation({
       query: () => ({
-        url: '/opportunities',
+        url: "/opportunities",
         // headers: {
         //   'Content-type': 'application/json; charset=UTF-8',
         // },
-      })
+      }),
     }),
-    getClients: builder.query({
-      query : () => ({url: '/clients'})
-    })
+    getClients: builder.mutation({
+      query: () => ({
+        url: "/opportunities",
+        // headers: {
+        //   'Content-type': 'application/json; charset=UTF-8',
+        // },
+      }),
+    }),
   }),
-})
+});
 
-export const { useSignInMutation, useGetOpportunitiesQuery, useGetClientsMutation} = jarvisApi
+export const {
+  useSignInMutation,
+  useGetOpportunitiesQuery,
+  useGetClientsMutation,
+} = jarvisApi;
