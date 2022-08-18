70dc1b4f47835c68c78e22ac49c94cb7415a1954
Haris Ali <haris.ali@empglabs.com>
2022-08-18 17:23:18 +0500

    Added Multi Select Components

--- src/Components/Client/Clients.js
+++ src/Components/Client/Clients.js
@@ -4,9 +4,23 @@ import {useEffect, useState} from "react";
 import JarvisNavbar from "../../Layout/JarvisNavbar";
 import {Link} from "react-router-dom";
 import {Spinner} from "react-bootstrap";
+import { MultiSelect } from "react-multi-select-component";
 
 const Clients = () => {
   const [getClients, { data }] = useGetClientsMutation();
+  const [clientType, setClientType] = useState([]);
+  const [classification, setClassification] = useState([]);
+  const [salesChannel, setSalesChannel] = useState([]);
+  const [productCategory, setProductCategory] = useState([]);
+  const [selected, setSelected] = useState([]);
+
+
+  const pushClientType = (params, arr) => {
+    params.map((item) => {
+      arr.push({value: item.id, label: item.name})
+    });
+    return arr;
+  }
 
   const getData = async () => {
     try {
@@ -17,16 +31,49 @@ const Clients = () => {
     }
   }
 
+  const DisplayMultiSelect = (arr, arg) => {
+    return (
+    <div>
+      <h2>{arg}</h2>
+      <MultiSelect
+        options={arr}
+        value={selected}
+        onChange={setSelected}
+        labelledBy="Select"
+      />
+    </div>
+    )
+  }
+
   useEffect(() => {
     if (!data?.success === true) getData();
     else {
-      console.log(data)
+      console.log("\nData: ", data)
+      if (clientType.length === 0) {
+        pushClientType(data.client_types, clientType, setClientType);
+        pushClientType(data.classifications, classification, setClassification);
+        pushClientType(data.sales_channels, salesChannel, setSalesChannel);
+        pushClientType(data.product_categories, productCategory, setProductCategory);
+        console.log("\nOwn Client: ", clientType);
+        console.log("\nOwn Classification: ", classification);
+        console.log("\nOwn Sales Channel: ", salesChannel);
+      }console.log("\nOwn Prod Cats: ", productCategory);
     }
   }, [data]);
 
   return (
     <>
       <JarvisNavbar />
+      <pre>{selected.map((item) => (
+        <p>
+          {item.label}
+        </p>
+      ))}</pre>
+      {DisplayMultiSelect(clientType, "Client Type")}
+      {DisplayMultiSelect(classification, "Classification")}
+      {DisplayMultiSelect(salesChannel, "Sales Channel")}
+      {DisplayMultiSelect(productCategory, "Product Categories")}
+      <br /><br />
     {!data?.success === true ? <><Spinner animation="border" role="status">
       </Spinner>
         <h5>Loading...</h5> </> :
