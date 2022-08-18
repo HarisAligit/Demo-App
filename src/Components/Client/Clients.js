0f9a10b5527078b306f3d3f1a86ae761b3fefcda
Haris Ali <haris.ali@empglabs.com>
2022-08-18 20:04:43 +0500

    Added Business Name input tag

--- src/Components/Client/Clients.js
+++ src/Components/Client/Clients.js
@@ -3,7 +3,7 @@ import { useGetClientsMutation } from "../../Redux/Slice/authSlice";
 import {useEffect, useState} from "react";
 import JarvisNavbar from "../../Layout/JarvisNavbar";
 import {Link} from "react-router-dom";
-import {Spinner} from "react-bootstrap";
+import {Button, Spinner} from "react-bootstrap";
 import { MultiSelect } from "react-multi-select-component";
 
 const Clients = () => {
@@ -13,23 +13,55 @@ const Clients = () => {
   const [salesChannel, setSalesChannel] = useState([]);
   const [productCategory, setProductCategory] = useState([]);
   const [selected, setSelected] = useState([]);
+  const [args, setArgs] = useState("false");
+  const [referenceNumberInput, setReferenceNumberInput] = useState(null);
+  const [businessName, setBusinessName] = useState('');
 
-
-  const pushClientType = (params, arr) => {
+  const pushFilterType = (params, arr, arg) => {
     params.map((item) => {
-      arr.push({value: item.id, label: item.name})
+      arr.push({value: item.id, label: item.name, arg: arg, inputType: 1})
     });
     return arr;
   }
 
   const getData = async () => {
     try {
-      await getClients().unwrap();
+      if (args === "false") {
+        await getClients().unwrap();
+      }
+      else {
+        await getClients(args).unwrap();
+      }
     }
     catch (err) {
       console.log(`Error: ${err}`)
     }
   }
+
+  useEffect(() => {
+    getData();
+  }, [args])
+
+  useEffect(() => {
+    if (selected.length === 0)
+    {
+      setArgs("false");
+    }
+    else {
+       let newArgs = "?";
+       console.log("\nSelected in arguments", selected);
+       selected.map((item) => {
+         if (item.inputType === 1) {
+           newArgs += "f" + "[" + item.arg + ".id]" + "[]=" + item.value + "&";
+         }
+         else if (item.inputType === 2) {
+           newArgs += "s" + "[" + item.arg + "]=" + item.value + "&";
+         }
+       });
+       setArgs(newArgs.slice(0, -1));
+       console.log("\nArguments:", args, "\n");
+    }
+  }, [selected])
 
   const DisplayMultiSelect = (arr, arg) => {
     return (
@@ -50,17 +82,30 @@ const Clients = () => {
     else {
       console.log("\nData: ", data)
       if (clientType.length === 0) {
-        pushClientType(data.client_types, clientType, setClientType);
-        pushClientType(data.classifications, classification, setClassification);
-        pushClientType(data.sales_channels, salesChannel, setSalesChannel);
-        pushClientType(data.product_categories, productCategory, setProductCategory);
+        pushFilterType(data.client_types, clientType, "client_type");
+        pushFilterType(data.classifications, classification, "classification");
+        pushFilterType(data.sales_channels, salesChannel, "sales_channel");
+        pushFilterType(data.product_categories, productCategory, "product_categories");
         console.log("\nOwn Client: ", clientType);
         console.log("\nOwn Classification: ", classification);
         console.log("\nOwn Sales Channel: ", salesChannel);
-      }console.log("\nOwn Prod Cats: ", productCategory);
+        console.log("\nOwn Prod Cats: ", productCategory);
+      }
     }
-  }, [data]);
+  }, [data, args]);
 
+  const handleBusiness = (e) => {
+     if (e.key === 'Enter') {
+       console.log("\nEnter Business Name: ", businessName);
+       setSelected(selected => [...selected, {value: businessName, arg: "name", inputType: 2}]);
+       console.log("new SElect", selected);
+     }
+     else {
+       setBusinessName(e.target.value)
+     }
+  }
+
+  // f[product_categories.id][]=5&f[sales_channel.id][]=2&f[client_type.id][]=4&f[classification.id][]=4
   return (
     <>
       <JarvisNavbar />
@@ -69,6 +114,11 @@ const Clients = () => {
           {item.label}
         </p>
       ))}</pre>
+
+      <Button>Reset Filters</Button>
+
+      <h3>Business Name</h3>
+      <input type="text" onKeyDown={handleBusiness}></input>
       {DisplayMultiSelect(clientType, "Client Type")}
       {DisplayMultiSelect(classification, "Classification")}
       {DisplayMultiSelect(salesChannel, "Sales Channel")}
